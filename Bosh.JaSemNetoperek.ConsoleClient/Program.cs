fce8ae74761afa9c52564efe69cb4adb9972a65c
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-11 13:45:44 +0200

    Utworzenie klienta

---
+++ Bosh.JaSemNetoperek.ConsoleClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Bosch.JaSemNetoperek.DAL;
+using Bosch.JaSemNetoperek.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bosh.JaSemNetoperek.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AddUserTest();
+        }
+
+        private static void AddUserTest()
+        {
+            var user = new User
+            {
+                FirstName = "Marcin",
+                LastName = "Sulecki"
+            };
+
+            using (var context = new NetoperekContext())
+            {
+                context.Users.Add(user);
+
+                context.SaveChanges();
+            }
+
+
+
+
+
+
+        }
+    }
+}
