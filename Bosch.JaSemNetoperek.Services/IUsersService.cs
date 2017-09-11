de5666c9336fc3e293bd20211818891f1c42748a
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-11 17:07:22 +0200

    Dodanie interfejsów

---
+++ Bosch.JaSemNetoperek.Services/IUsersService.cs
@@ -0,0 +1,36 @@
+﻿using Bosch.JaSemNetoperek.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bosch.JaSemNetoperek.Services
+{
+    public interface IBaseService<T>
+        where T : class
+    {
+        void Add(T item);
+
+        IEnumerable<T> Get();
+
+        //void Update(T item);
+
+        //void Remove(int id);
+
+        T Get(int id);
+
+
+    }
+
+    public interface IUsersService : IBaseService<User>
+    {
+    }
+
+    public interface IStationsService : IBaseService<Station>
+    {
+    }
+
+    public interface IRentalsService : IBaseService<Rental>
+    {
+
+    }
+}
