b4c191e4b36c79ae2a1365af90472bb85540353c
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-11 12:12:51 +0200

    Utworzenie DAL

---
+++ Bosch.JaSemNetoperek.DAL/NetoperekContext.cs
@@ -0,0 +1,30 @@
+﻿using Bosch.JaSemNetoperek.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bosch.JaSemNetoperek.DAL
+{
+    public class NetoperekContext : DbContext
+    {
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public DbSet<Station> Stations { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Rental> Rentals { get; set; }
+
+
+        public NetoperekContext()
+            : base("NetoperekConnection")
+        {
+
+        }
+
+    }
+}
