da271f5ee123bea9d6d613f0854d78bff35abcd2
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-12 16:26:34 +0200

    Dodanie implementacji pobierania lokalizacji

--- Bosch.JaSemNetoperek.DbServices/DbUsersService.cs
+++ Bosch.JaSemNetoperek.DbServices/DbUsersService.cs
@@ -1,5 +1,6 @@
 ﻿using Bosch.JaSemNetoperek.Models;
 using Bosch.JaSemNetoperek.Services;
+using System.Linq;
 using System;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
@@ -19,7 +20,37 @@ namespace Bosch.JaSemNetoperek.DbServices
     {
         public Station GetByLocation(Location location)
         {
-            throw new NotImplementedException();
+            var station = context.Stations
+                .FirstOrDefault(s => s.Location == location);
+
+            return station;
+
+            //IQueryable<Station> nearStations = context.Stations
+            //    .Where(s => s.Location == location)
+            //    .Where(s => s.Capacity > 10);
+
+            //foreach (var station in nearStations)
+            //{
+
+            //}
+
+
+            //var nearStations2 = context.Stations
+            //    .Where(s => s.Location == location)
+            //    .Where(s => s.Capacity > 10);
+                
+
+            //foreach (var station in nearStations2)
+            //{
+
+            //}
+
+
+            //var q = context.Rentals
+            //    .GroupBy(r => r.FromStation)
+            //    .Select(r => new { Station = r.Key, Quantity = r.Count() } );
+
+
         }
     }
 }
