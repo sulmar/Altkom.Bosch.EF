b6a23588bfd99a5ee22dd7062f6e422a6c277b82
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-12 21:24:54 +0200

    Dodanie migracji

---
+++ Bosch.JaSemNetoperek.DALCore/Migrations/20170912190213_InitialMigration.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Bosch.JaSemNetoperek.DALCore.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stations",
+                columns: table => new
+                {
+                    StationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Capacity = table.Column<byte>(type: "tinyint", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Location_Latitude = table.Column<float>(type: "real", nullable: false),
+                    Location_Longitude = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stations", x => x.StationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    EngineCapacity = table.Column<float>(type: "real", nullable: true),
+                    EngineType = table.Column<int>(type: "int", nullable: true),
+                    MaxSpeed = table.Column<int>(type: "int", nullable: true),
+                    HasBidon = table.Column<bool>(type: "bit", nullable: true),
+                    Car_EngineCapacity = table.Column<float>(type: "real", nullable: true),
+                    Car_EngineType = table.Column<int>(type: "int", nullable: true),
+                    Car_MaxSpeed = table.Column<int>(type: "int", nullable: true),
+                    MotorBike_EngineCapacity = table.Column<float>(type: "real", nullable: true),
+                    MotorBike_EngineType = table.Column<int>(type: "int", nullable: true),
+                    MotorBike_MaxSpeed = table.Column<int>(type: "int", nullable: true),
+                    VehicleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    StationId = table.Column<int>(type: "int", nullable: true),
+                    VehicleType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location_Latitude = table.Column<float>(type: "real", nullable: false),
+                    Location_Longitude = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.VehicleId);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Stations_StationId",
+                        column: x => x.StationId,
+                        principalTable: "Stations",
+                        principalColumn: "StationId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rentals",
+                columns: table => new
+                {
+                    RentalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cost = table.Column<decimal>(type: "decimal(18, 2)", nullable: true),
+                    FromDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FromStationStationId = table.Column<int>(type: "int", nullable: true),
+                    RenteeUserId = table.Column<int>(type: "int", nullable: true),
+                    ToDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ToStationStationId = table.Column<int>(type: "int", nullable: true),
+                    VehicleId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rentals", x => x.RentalId);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Stations_FromStationStationId",
+                        column: x => x.FromStationStationId,
+                        principalTable: "Stations",
+                        principalColumn: "StationId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Users_RenteeUserId",
+                        column: x => x.RenteeUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Stations_ToStationStationId",
+                        column: x => x.ToStationStationId,
+                        principalTable: "Stations",
+                        principalColumn: "StationId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rentals_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "VehicleId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_FromStationStationId",
+                table: "Rentals",
+                column: "FromStationStationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_RenteeUserId",
+                table: "Rentals",
+                column: "RenteeUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_ToStationStationId",
+                table: "Rentals",
+                column: "ToStationStationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_VehicleId",
+                table: "Rentals",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_StationId",
+                table: "Vehicles",
+                column: "StationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rentals");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "Stations");
+        }
+    }
+}
